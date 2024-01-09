8865527c0147365f64dfedd1601eb368190a6e7e
tschoemaker <=>
2024-01-09 17:14:03 +0100

    further improved evaluation

--- src/llm_evaluator.ts
+++ src/llm_evaluator.ts
@@ -5,58 +5,57 @@ import { getRelevantFilesRec } from "./util/Utils";
 import { FileFilteringContext } from "./context/DataContext";
 import { ChatMessage } from "./util/languageModel/LanguageModelInterface";
 
-const ground_truth=JSON.parse(fs.readFileSync("llm_results/dataClumpDetectorContext.json",{encoding:"utf-8"}))
-function standardize(dcData:DataClumpsTypeContext){
-    let result:DataClumpsTypeContext={} as any
-    result.data_clumps={}
-    for(let dcKey of Object.keys(dcData.data_clumps)){
-        let dcValue=dcData.data_clumps[dcKey];
-        if(dcValue["key"]==undefined)continue
-        dcValue.key=undefined as any
-        dcValue.from_method_key=undefined as any
-        dcValue.to_method_key=undefined as any
-        let newDCData={}
+const ground_truth = JSON.parse(fs.readFileSync("llm_results/dataClumpDetectorContext.json", { encoding: "utf-8" }))
+function standardize(dcData: DataClumpsTypeContext) {
+    let result: DataClumpsTypeContext = {} as any
+    result.data_clumps = {}
+    for (let dcKey of Object.keys(dcData.data_clumps)) {
+        let dcValue = dcData.data_clumps[dcKey];
+        if (dcValue["key"] == undefined) continue
+        dcValue.key = undefined as any
+        dcValue.from_method_key = undefined as any
+        dcValue.to_method_key = undefined as any
+        let newDCData = {}
 
-        dcValue.from_class_or_interface_key=undefined as any
-        dcValue.to_class_or_interface_key=undefined as any
+        dcValue.from_class_or_interface_key = undefined as any
+        dcValue.to_class_or_interface_key = undefined as any
 
-        for( let dcDataKey of Object.keys(dcValue.data_clump_data)){
-            let dcDataValue=dcValue.data_clump_data[dcDataKey]
-            dcDataValue.to_variable.key=undefined as any
-            dcDataValue.key=undefined as any
-            let hashed=sha256(JSON.stringify(dcDataValue))
-            dcDataValue.key=hashed
-            newDCData[hashed]=dcDataValue
+        for (let dcDataKey of Object.keys(dcValue.data_clump_data)) {
+            let dcDataValue = dcValue.data_clump_data[dcDataKey]
+            dcDataValue.to_variable.key = undefined as any
+            dcDataValue.key = undefined as any
+            let hashed = sha256(JSON.stringify(dcDataValue))
+            dcDataValue.key = hashed
+            newDCData[hashed] = dcDataValue
 
         }
-        dcValue.data_clump_data=newDCData
-        let hashed=sha256(JSON.stringify(dcValue))
-        dcValue.key=hashed
-      result.data_clumps[hashed]=dcValue
+        dcValue.data_clump_data = newDCData
+        let hashed = sha256(JSON.stringify(dcValue))
+        dcValue.key = hashed
+        result.data_clumps[hashed] = dcValue
 
     }
     return result
 }
-function main(){
-}
-function get_output_file_paths():string[]{
-    let result=[]
-    getRelevantFilesRec("llm_results",result,new FileFilteringContext(["*output.json"],[]))
+
+function get_output_file_paths(): string[] {
+    let result = []
+    getRelevantFilesRec("llm_results", result, new FileFilteringContext(["*output.json"], []))
     return result;
 }
-function parse_chat_file(path:string){
-    let chat=JSON.parse(fs.readFileSync(path,{encoding:"utf-8"})) as ChatMessage[]
-    let outputs=chat.filter((x)=>x.messageType=="output" && x.messages.length>0 && x.messages[0]["data_clumps"]!=undefined);
-    let combined:DataClumpsTypeContext={
-        data_clumps:{}
+function parse_chat_file(path: string) {
+    let chat = JSON.parse(fs.readFileSync(path, { encoding: "utf-8" })) as ChatMessage[]
+    let outputs = chat.filter((x) => x.messageType == "output" && x.messages.length > 0 && x.messages[0]["data_clumps"] != undefined);
+    let combined: DataClumpsTypeContext = {
+        data_clumps: {}
     } as any;
-    let tooLarge=outputs.some((x)=>x.messages.length>1)
-    for(let output of outputs){
-        let msg=output.messages[0] as any as DataClumpsTypeContext
-        let standardized=standardize(msg)
+    let tooLarge = outputs.some((x) => x.messages.length > 1)
+    for (let output of outputs) {
+        let msg = output.messages[0] as any as DataClumpsTypeContext
+        let standardized = standardize(msg)
 
-        for(let key of Object.keys(standardized.data_clumps)){
-            combined.data_clumps[key]=standardized.data_clumps[key]
+        for (let key of Object.keys(standardized.data_clumps)) {
+            combined.data_clumps[key] = standardized.data_clumps[key]
         }
 
     }
@@ -64,73 +63,124 @@ function parse_chat_file(path:string){
     return combined
 
 }
-function sha256(content:string):string {  
+function sha256(content: string): string {
     return createHash('sha256').update(content).digest('hex')
 }
-function get_class_method_tuples(dcContext:DataClumpsTypeContext):string[]{
-    let detectedClassMethodPairs:string[]=[]
-    for(let dcKey of Object.keys(dcContext.data_clumps)){
-        let dc=dcContext.data_clumps[dcKey]
-        let class_identifier=[dc.from_class_or_interface_name,dc.to_class_or_interface_name].sort()
-        if(class_identifier.includes("X_y_z") || class_identifier.includes("Exponent_mantissa_sign")){
+function get_class_method_tuples(dcContext: DataClumpsTypeContext): string[] {
+    let detectedClassMethodPairs: string[] = []
+    for (let dcKey of Object.keys(dcContext.data_clumps)) {
+        let dc = dcContext.data_clumps[dcKey]
+        let class_identifier = [dc.from_class_or_interface_name, dc.to_class_or_interface_name].sort()
+        if (class_identifier.includes("X_y_z") || class_identifier.includes("Exponent_mantissa_sign")) {
             continue;
         }
-        let method_identifier=[dc.from_method_name,dc.to_method_name].sort()
-        let all=[...class_identifier,...method_identifier]
-        let stringified=JSON.stringify(all)
+        let method_identifier = [dc.from_method_name, dc.to_method_name].sort()
+        let all = [...class_identifier, ...method_identifier]
+        let stringified = JSON.stringify(all)
         detectedClassMethodPairs.push(stringified)
 
     }
     return detectedClassMethodPairs;
 }
-let ground_truth_standardized=standardize(ground_truth)
-let paths=get_output_file_paths();
-let max_d_in_o=0;
-let max_o_in_d=0;
-let max_d_in_o_path=""
-let max_o_in_d_path=""
-console.log(paths)
-let original=get_class_method_tuples(ground_truth_standardized)
-for(let p of paths){
-    let combined=parse_chat_file(p)
-    let detected=get_class_method_tuples(combined)
-    let original_in_detected=0;
-    let detected_in_original=0;
-    for(let o of original){
-        if(detected.includes(o)){
+let ground_truth_standardized = standardize(ground_truth)
+let original = get_class_method_tuples(ground_truth_standardized)
 
-            original_in_detected++;
+function findBestMethod(paths: string[]) {
+    let max_d_in_o = 0;
+    let max_o_in_d = 0;
+    let max_d_in_o_path = ""
+    let max_o_in_d_path = ""
+
+    let min_d_in_o = 1;
+    let min_o_in_d = 1;
+    let min_d_in_o_path = ""
+    let min_o_in_d_path = ""
+    for (let p of paths) {
+        let combined = parse_chat_file(p)
+        let detected = get_class_method_tuples(combined)
+        let original_in_detected = 0;
+        let detected_in_original = 0;
+        for (let o of original) {
+            if (detected.includes(o)) {
+
+                original_in_detected++;
+            }
+            else {
+                //console.log(p, "Unknown in detected", o)
+
+            }
         }
-        else{
-            console.log(p,"Unknown in detected",o)
+        for (let d of detected) {
+            if (original.includes(d)) {
+                detected_in_original++;
+            }
+            else {
+                //console.log(p, "Unknown in original", d)
+
+            }
+        }
+        detected_in_original /= detected.length
+        original_in_detected /= original.length
+        if (detected_in_original > max_d_in_o ) {
+            max_d_in_o = detected_in_original
+            max_d_in_o_path = p
 
         }
-    }
-    for(let d of detected){
-        if(original.includes(d)){
-            detected_in_original++;
+        if (original_in_detected > max_o_in_d ) {
+            max_o_in_d = original_in_detected
+            max_o_in_d_path = p
         }
-        else{
-            console.log(p,"Unknown in original",d)
+        if (detected_in_original < min_d_in_o) {
+            min_d_in_o = detected_in_original
+            min_d_in_o_path = p
 
         }
-    }
-    detected_in_original/=detected.length
-    original_in_detected/=original.length
-    if(detected_in_original>max_d_in_o && p.includes("source")){
-        max_d_in_o=detected_in_original
-        max_d_in_o_path=p
-    
-    }
-    if(original_in_detected>max_o_in_d && p.includes("source")){
-        max_o_in_d=original_in_detected
-        max_o_in_d_path=p
-    }
-    console.log(p,"D in O",detected_in_original*100,"%","O in D",original_in_detected*100,"%")
+        if (original_in_detected < min_o_in_d ) {
+            min_o_in_d = original_in_detected
+            min_o_in_d_path = p
+        }
 
-    
+
+    }
+    return {
+        SensitivityBest: max_d_in_o*100,
+        SpecifityBest:max_o_in_d*100,
+        SensitivityBestPath:max_d_in_o_path,
+        SpecifityBestPath:max_o_in_d_path,
+        whiteSpace:"##############################################################################",
+        SensitivityWorst: min_d_in_o*100,
+        SpecifityWorst:min_o_in_d*100,
+        SensitivityWorstPath:min_d_in_o_path,
+        SpecifityWorstPath:min_o_in_d_path,
+        
+    }
 }
-console.log()
-console.log();
-console.log("Max D in O",max_d_in_o*100,"%",max_d_in_o_path)
-console.log("Max O in D",max_o_in_d*100,"%",max_o_in_d_path)
+function main() {
+    let paths = get_output_file_paths()
+    const filters={
+        "All":(x:string)=>true,
+        "Source":(x:string)=>x.includes("source"),
+        "ast":(x:string)=>x.includes("ast"),
+        "pairOfFileAndSingle":(x:string)=>x.includes("PairOfFileAndSingle"),
+        "AllFiles":(x:string)=>x.includes("AllFiles"),
+        "gpt-4":(x:string)=>x.includes("gpt-4"),
+        "gpt-3":(x:string)=>x.includes("gpt-3"),
+        "definitionBased":(x:string)=>x.includes("definitionBased"),
+        "exampleBased":(x:string)=>x.includes("exampleBased"),
+        "noDefinition":(x:string)=>x.includes("noDefinition"),
+
+    }
+    let evalResult={}
+    for(let filterKey of Object.keys(filters)){
+        console.log(filterKey)
+        let result=findBestMethod(paths.filter(filters[filterKey]))
+       evalResult[filterKey]=result
+       console.log(result)
+       console.log()
+    }
+    fs.writeFileSync("llm_results/evaluator.json",JSON.stringify(evalResult,null,2))
+}
+main();
+
+
+
