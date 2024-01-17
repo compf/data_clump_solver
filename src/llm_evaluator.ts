13c5939686f249ee53f51d7bc49e47c640251231
tschoemaker <=>
2024-01-17 23:14:42 +0100

    some more eval results

--- src/llm_evaluator.ts
+++ src/llm_evaluator.ts
@@ -22,7 +22,7 @@ function standardize(dcData: DataClumpsTypeContext) {
 
         for (let dcDataKey of Object.keys(dcValue.data_clump_data)) {
             let dcDataValue = dcValue.data_clump_data[dcDataKey]
-            if(dcDataValue.to_variable){
+            if (dcDataValue.to_variable) {
                 dcDataValue.to_variable.key = undefined as any
             }
             dcDataValue.key = undefined as any
@@ -86,24 +86,24 @@ function get_class_method_tuples(dcContext: DataClumpsTypeContext): string[] {
 }
 let ground_truth_standardized = standardize(ground_truth)
 let original = get_class_method_tuples(ground_truth_standardized)
-function median(array:number[]){
-    array.sort((a,b)=>a-b)
-    if(array.length%2==0){
-        return (array[array.length/2]+array[array.length/2-1])/2
+function median(array: number[]) {
+    array.sort((a, b) => a - b)
+    if (array.length % 2 == 0) {
+        return (array[array.length / 2] + array[array.length / 2 - 1]) / 2
     }
-    else{
-        return array[Math.floor(array.length/2)]
+    else {
+        return array[Math.floor(array.length / 2)]
     }
 }
-function mean(array:number[]){
-    let result=array.reduce((a,b)=>a+b,0)/array.length
-    if(isNaN(result)){
+function mean(array: number[]) {
+    let result = array.reduce((a, b) => a + b, 0) / array.length
+    if (isNaN(result)) {
         console.log(array)
-        for( let i of array){
+        for (let i of array) {
             console.log(i)
         }
-        console.log("sum",array.reduce((a,b)=>a+b,0))
-        console.log("length",array.length)
+        console.log("sum", array.reduce((a, b) => a + b, 0))
+        console.log("length", array.length)
         //throw ""
     }
     return result;
@@ -118,8 +118,8 @@ function findBestMethod(paths: string[]) {
     let min_o_in_d = 1;
     let min_d_in_o_path = ""
     let min_o_in_d_path = ""
-    let originalInDetected:number[]=[]
-    let detectedInOriginal:number[]=[]
+    let originalInDetected: number[] = []
+    let detectedInOriginal: number[] = []
 
     for (let p of paths) {
         let combined = parse_chat_file(p)
@@ -145,21 +145,21 @@ function findBestMethod(paths: string[]) {
 
             }
         }
-        if(detected.length>0){
+        if (detected.length > 0) {
             detected_in_original /= detected.length
 
         }
-        if(original.length>0){
+        if (original.length > 0) {
             original_in_detected /= original.length
         }
         originalInDetected.push(original_in_detected)
         detectedInOriginal.push(detected_in_original)
-        if (detected_in_original > max_d_in_o ) {
+        if (detected_in_original > max_d_in_o) {
             max_d_in_o = detected_in_original
             max_d_in_o_path = p
 
         }
-        if (original_in_detected > max_o_in_d ) {
+        if (original_in_detected > max_o_in_d) {
             max_o_in_d = original_in_detected
             max_o_in_d_path = p
         }
@@ -168,7 +168,7 @@ function findBestMethod(paths: string[]) {
             min_d_in_o_path = p
 
         }
-        if (original_in_detected < min_o_in_d ) {
+        if (original_in_detected < min_o_in_d) {
             min_o_in_d = original_in_detected
             min_o_in_d_path = p
         }
@@ -176,74 +176,77 @@ function findBestMethod(paths: string[]) {
 
     }
     return {
-        SensitivityBest: max_d_in_o*100,
-        SpecifityBest:max_o_in_d*100,
-        SensitivityBestPath:max_d_in_o_path,
-        SpecifityBestPath:max_o_in_d_path,
-        whiteSpace:"##############################################################################",
-        SensitivityWorst: min_d_in_o*100,
-        SpecifityWorst:min_o_in_d*100,
-        SensitivityWorstPath:min_d_in_o_path,
-        SpecifityWorstPath:min_o_in_d_path,
-        whiteSpace2:"##############################################################################",
-        medianSensitivity:median(detectedInOriginal)*100,
-        medianSpecifity:median(originalInDetected)*100,
-        meanSensitivity:mean(detectedInOriginal)*100,
-        meanSpecifity:mean(originalInDetected)*100,
-        
+        SensitivityBest: max_d_in_o * 100,
+        SpecifityBest: max_o_in_d * 100,
+        SensitivityBestPath: max_d_in_o_path,
+        SpecifityBestPath: max_o_in_d_path,
+        sensitivies: detectedInOriginal.map((it) => 100 * it),
+        specifities: originalInDetected.map((it) => it * 100),
+        whiteSpace: "##############################################################################",
+        SensitivityWorst: min_d_in_o * 100,
+        SpecifityWorst: min_o_in_d * 100,
+        SensitivityWorstPath: min_d_in_o_path,
+        SpecifityWorstPath: min_o_in_d_path,
+        whiteSpace2: "##############################################################################",
+        medianSensitivity: median(detectedInOriginal) * 100,
+        medianSpecifity: median(originalInDetected) * 100,
+        meanSensitivity: mean(detectedInOriginal) * 100,
+        meanSpecifity: mean(originalInDetected) * 100,
+
     }
 }
-const dataTypeFilters={
-    "Source":(x:string)=>x.includes("source"),
-    "ast":(x:string)=>x.includes("ast"),
-  
-}
-const dataSizeFilters={
-    "AllFiles":(x:string)=>x.includes("AllFiles"),
-    "PairOfFileAndSingle":(x:string)=>x.includes("PairOfFileAndSingle")
-}
-const apiFilters={
-    "gpt-4":(x:string)=>x.includes("gpt-4"),
-    "gpt-3":(x:string)=>x.includes("gpt-3"),
-}
-const  temperatureFilters={
-    "0.1":(x:string)=>x.includes("0.1"),
-    "0.9":(x:string)=>x.includes("0.9"),
-}
-const instructionFilters={
-    "definitionBased":(x:string)=>x.includes("definitionBased"),
-    "exampleBased":(x:string)=>x.includes("exampleBased"),
-    "noDefinition":(x:string)=>x.includes("noDefinition"),
-}
-function main() {
-    let evalResult={}
-    let allPaths=get_output_file_paths()
-    let filtered=allPaths;
-    for(let apiFilterKey of Object.keys(apiFilters)){
+const dataTypeFilters = {
+    "Source": (x: string) => x.includes("source"),
+    "ast": (x: string) => x.includes("ast"),
 
-        filtered=allPaths
-        filtered=filtered.filter(apiFilters[apiFilterKey])
-        evalResult[apiFilterKey]={all:findBestMethod(allPaths.filter(apiFilters[apiFilterKey]))}
+}
+const dataSizeFilters = {
+    "AllFiles": (x: string) => x.includes("AllFiles"),
+    "PairOfFileAndSingle": (x: string) => x.includes("PairOfFileAndSingle")
+}
+const apiFilters = {
+    "gpt-4": (x: string) => x.includes("gpt-4"),
+    "gpt-3": (x: string) => x.includes("gpt-3"),
+}
+const temperatureFilters = {
+    "0.1": (x: string) => x.includes("0.1"),
+    "0.9": (x: string) => x.includes("0.9"),
+}
+const instructionFilters = {
+    "definitionBased": (x: string) => x.includes("definitionBased"),
+    "exampleBased": (x: string) => x.includes("exampleBased"),
+    "noDefinition": (x: string) => x.includes("noDefinition"),
+}
+let filtersPermutations = {
+    "api_temp_instr_dataType_data_Size":[apiFilters, temperatureFilters, instructionFilters, dataTypeFilters, dataSizeFilters],
+    "dataSize_dataType_instr_temp_api":[apiFilters, temperatureFilters, instructionFilters, dataTypeFilters, dataSizeFilters].reverse(),
+
+}
+function create_evaluation(key:string,permutation: any[]) {
+    let evalResult = {}
+    for (let key0 of Object.keys(permutation[0])) {
         
-        for(let temperatureFilterKey of Object.keys(temperatureFilters)){
 
-            evalResult[apiFilterKey][temperatureFilterKey]={all:findBestMethod(allPaths.filter(apiFilters[apiFilterKey]).filter(temperatureFilters[temperatureFilterKey]))}
-            
-            for(let instructionFilterKey of Object.keys(instructionFilters)){
+        let allPaths = get_output_file_paths()
+        evalResult[key0] = { all: findBestMethod(allPaths.filter(permutation[0][key0])) }
 
-                evalResult[apiFilterKey][temperatureFilterKey][instructionFilterKey]={all:findBestMethod(allPaths.filter(apiFilters[apiFilterKey]).filter(temperatureFilters[temperatureFilterKey]).filter(instructionFilters[instructionFilterKey]))}
-                
-                for(let dataTypeFilterKey of Object.keys(dataTypeFilters)){
+        for (let key1 of Object.keys(permutation[1])) {
 
-                  
-                    evalResult[apiFilterKey][temperatureFilterKey][instructionFilterKey][dataTypeFilterKey]={all:findBestMethod(allPaths.filter(apiFilters[apiFilterKey]).filter(temperatureFilters[temperatureFilterKey]).filter(instructionFilters[instructionFilterKey]).filter(dataTypeFilters[dataTypeFilterKey]))}
-                    
-                    for(let dataSizeFilterKey of Object.keys(dataSizeFilters)){
-                        let paths = allPaths.filter(dataTypeFilters[dataTypeFilterKey]).filter(dataSizeFilters[dataSizeFilterKey]).filter(apiFilters[apiFilterKey]).filter(temperatureFilters[temperatureFilterKey]).filter(instructionFilters[instructionFilterKey])
-                        const keyCombined=apiFilterKey+"/"+temperatureFilterKey+"_/"+instructionFilterKey+"/"+dataTypeFilterKey+"/"+dataSizeFilterKey
-                        console.log(dataTypeFilterKey,dataSizeFilterKey,apiFilterKey,temperatureFilterKey,instructionFilterKey)
-                       let result= findBestMethod(paths)
-                        evalResult[apiFilterKey][temperatureFilterKey][instructionFilterKey][dataTypeFilterKey][dataSizeFilterKey]=result;
+            evalResult[key0][key1] = { all: findBestMethod(allPaths.filter(permutation[0][key0]).filter(permutation[1][key1])) }
+
+            for (let key2 of Object.keys(permutation[2])) {
+
+                evalResult[key0][key1][key2] = { all: findBestMethod(allPaths.filter(permutation[0][key0]).filter(permutation[1][key1]).filter(permutation[2][key2])) }
+
+                for (let key3 of Object.keys(permutation[3])) {
+
+
+                    evalResult[key0][key1][key2][key3] = { all: findBestMethod(allPaths.filter(permutation[0][key0]).filter(permutation[1][key1]).filter(permutation[2][key2]).filter(permutation[3][key3])) }
+
+                    for (let key4 of Object.keys(permutation[4])) {
+                        let paths = allPaths.filter(permutation[0][key0]).filter(permutation[1][key1]).filter(permutation[2][key2]).filter(permutation[3][key3]).filter(permutation[4][key4])
+                        let result = findBestMethod(paths)
+                        evalResult[key0][key1][key2][key3][key4] = result;
 
                         console.log()
                     }
@@ -251,8 +254,14 @@ function main() {
             }
         }
     }
-    
-    fs.writeFileSync("llm_results/evaluator.json",JSON.stringify(evalResult,null,2))
+
+    fs.writeFileSync("llm_results/"+key+".json", JSON.stringify(evalResult, null, 2))
+}
+function main() {
+    for(let key of Object.keys(filtersPermutations))   {
+        create_evaluation(key,filtersPermutations[key])
+    }
+
 }
 main();
 
