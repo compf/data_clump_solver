d3a9268b2dc87a290c33262dc81984d5fcc5f7f6
compf <timo.compf@outlook.de>
2024-09-02 23:44:35 +0000

    missin files

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
-#npm run evalRefactor  config_eval_refactor.json src/eval/urls.txt
-npm run evalDetect  config_eval_detect.json src/eval/urls.txt
-npm run evalFilter  config_eval_filter.json src/eval/urls.txt
+npm run evalRefactor  config_eval_refactor.json src/eval/urls.txt
+#npm run evalDetect  config_eval_detect.json src/eval/urls.txt
+#npm run evalFilter  config_eval_filter.json src/eval/urls.txt
