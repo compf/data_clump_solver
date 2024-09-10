4ee61f3b42e641c6d5284b316dcd6079f88b2e19
tschoemaker <tschoemaker@uni-osnabrueck.de>
2024-09-10 17:52:31 +0200

    added likert data and some updates

--- src/prData/structures.ts
+++ src/prData/structures.ts
@@ -1,13 +1,13 @@
 import fs from "fs";
-let counter=1;
-export const Readability=counter++;
-export const Complexity=counter++;
-export const ManualChanges=counter++;
-export const Intentional=counter++;
-export const Performance=counter++;
+let counter = 1;
+export const Readability = counter++;
+export const Complexity = counter++;
+export const ManualChanges = counter++;
+export const Intentional = counter++;
+export const Performance = counter++;
 export const Internal = counter++;
 export const NotWorthIt = counter++;
-export const  LongLines=counter++;
+export const LongLines = counter++;
 export const NotEnough = counter++;
 export const SmallerDataClump = counter++;
 export const LargerDataClump = counter++;
@@ -15,45 +15,62 @@ export const OverEngineered = counter++;
 export const DocumentationIssues = counter++;
 export const JavaRecord = counter++;
 export const ExtractedClassLocation = counter++;
-export const ResponseTooLate =counter++
-export const LLM_Useful =counter++
-export const ClassName =counter++;
-export const Imports =counter++;
-export const SemanticChanges =counter++;
-export const SameBaseClass =counter++;
-export const ExtractedClassPublic =counter++;
+export const ResponseTooLate = counter++
+export const LLM_Useful = counter++
+export const ClassName = counter++;
+export const Imports = counter++;
+export const SemanticChanges = counter++;
+export const SameBaseClass = counter++;
+export const ExtractedClassPublic = counter++;
 export const StyleAdaption = counter++;
 export const LicenseHeader = counter++;
 export const LLM_LegalIssues = counter++;
 export const Invalid = counter++;
+export const Maintainability = counter++;
+export const IsolatedComponents = counter++;
+export const LLM_DeveloperOversight = counter++;
+export const Good_Idea = counter++;
 
 
-export const parameters_to_parameters_data_clump="parameters_to_parameters_data_clump";
-export const fields_to_fields_data_clump="fields_to_fields_data_clump";
-export type PR_Data_Entry={
+export const parameters_to_parameters_data_clump = "parameters_to_parameters_data_clump";
+export const fields_to_fields_data_clump = "fields_to_fields_data_clump";
+export type PR_Data_Entry = {
 
-    url:string,
-    merged:boolean,
-    state:string,
-    type:string,
-    size:number,
-    noResponse?:boolean,
-    manualChanges?:boolean,
-    category:string
-    likertData?:{
-        scale:number,
-        comments:string[]
-    }[],
-    generalComments:number[],
-    reviewComments:number[],
-    generalCommentsRaw:string[][],
-    reviewCommentsRaw:string[][]
-   }
+    url: string,
+    merged: boolean,
+    state: string,
+    type: string,
+    size: number,
+    noResponse?: boolean,
+    manualChanges?: boolean,
+    category: string
+    likertData?: {
+        scale: number,
+        comments?: string,
+        keywords?: number[]
 
-
-export type PR_Data={
-    [key:string]:PR_Data_Entry
+    }[][],
+    experience?: {
+        project: string,
+        java: string
+    }[]
+    generalComments: number[],
+    reviewComments: number[],
+    generalCommentsRaw: string[][],
+    reviewCommentsRaw: string[][]
 }
 
 
+export type PR_Data = {
+    [key: string]: PR_Data_Entry
+}
+
+export const StronglyDisagree = 0;
+export const Disagree = 1;
+export const Neutral = 2;
+export const Agree = 3;
+export const StronglyAgree = 4;
+
+
+export const NEUTRAL_COMMENT = 0.5
 
