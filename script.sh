4143017f73923e570d1ea349071658b41e2a91bb
compf <timo.compf@outlook.de>
2024-10-01 08:35:23 +0000

    added detect and filter data for rocketmq

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
-npm run evalRefactor  config_eval_refactor.json src/eval/urls.txt
-#npm run evalDetect  config_eval_detect.json src/eval/urls.txt
-#npm run evalFilter  config_eval_filter.json src/eval/urls.txt
+#npm run evalRefactor  config_eval_refactor.json src/eval/urls.txt
+npm run evalDetect  https://gitlab.com/uni3193438/rocketmq_refactor
+npm run evalFilter  https://gitlab.com/uni3193438/rocketmq_refactor
